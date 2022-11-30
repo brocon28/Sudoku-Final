a6310ce50cc5cf94788bd331e9fc93a701a03036
gstrand17 <gstrand@ufl.edu>
2022-11-30 00:37:09 -0500

    began class, defined all functions

---
+++ board.py
@@ -0,0 +1,33 @@
+from sudoku_generator import *
+
+
+class Board:
+    def _init(self, width, height, screen, difficulty):
+        pass
+
+    def draw(self):
+        pass
+
+    def select(self, row, col):
+        pass
+
+    def click(self, x, y):
+        pass
+
+    def clear(self):
+        pass
+
+    def sketch(self, value):
+        pass
+
+    def place_number(self, value):
+        pass
+
+    def update_board(self):
+        pass
+
+    def find_empty(self):
+        pass
+
+    def check_board(self):
+        pass
