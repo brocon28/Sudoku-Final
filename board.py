e60d4cba9ad4c825396596718db11988bbfd1176
rileyeward <rileyward@ufl.edu>
2024-04-17 10:58:03 -0400

    updated is_full function, find_empty, and check_board. Also imported sudoku generator as SG

--- board.py
+++ board.py
@@ -1,3 +1,4 @@
+from sudoku_generator import SudokuGenerator as SG
 class Board:
     def __init__(self, width,height,screen,difficulty):
     def draw(self):
@@ -22,13 +23,33 @@ class Board:
         pass
 
     def is_full(self):
-        pass
+        total = 0
+        for i in range (0, 9, 1):
+            for j in range (0, 9, 1):
+                if board[i][j] > 0:
+                    total += 1
+        if total == 81:
+            return True
+        else:
+            return False
 
     def update_board(self):
         pass
 
     def find_empty(self):
-        pass
+        for i in range(0, 9, 1):
+            for j in range(0, 9, 1):
+                if board[i][j] == 0:
+                    print(str(i)+", "+str(j))
+
 
     def check_board(self):
-        pass
+        total=0
+        for i in range (0, 9, 1):
+            if SG.valid_in_row(i) is True and SG.valid_in_col(i) is True:
+                total += 1
+        if total == 9:
+            return True
+        else:
+            return False
+        #think this needs to pull the valid functions from sudoku_generator
