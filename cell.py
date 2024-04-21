447044a6b66a28d396f800d49975a22cfc89dcb2
Vivian Rincon <vivirincond@gmail.com>
2024-04-21 17:30:58 -0400

    Made and finalized the board

--- cell.py
+++ cell.py
@@ -15,13 +15,14 @@ class Cell:
     def set_sketched_value(self,value):
         self.value = value
 
-    def draw(self,screen):
+    def draw(self):
         BLACK=(0,0,0)
         WHITE=(255,255,255)
         RED=(255,0,0)
         GREEN=(0,255,0)
         BLUE=(0,0,255)
         num_font = pygame.font.SysFont('arial', 12)
+        #drawing the surface of each number
         n1_num=num_font.render("1", True, WHITE)
         n2_num=num_font.render("2", True, WHITE)
         n3_num=num_font.render("3", True, WHITE)
@@ -32,54 +33,54 @@ class Cell:
         n8_num=num_font.render("8", True, WHITE)
         n9_num=num_font.render("9", True, WHITE)
 
-        if self.touch==True:
-            pygame.draw.rect(self.screen, BLUE,pygame.Rect(self.column * SQUARE_SIZE, self.row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE),2)
+        #if self.touch==True:
+        #    pygame.draw.rect(self.screen, BLUE,pygame.Rect(self.column * SQUARE_SIZE, self.row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE),2)
 
         if self.value==1:
             num_rect=n1_num.get_rect(
-                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + self.row * SQUARE_SIZE//2))
-            screen.blit(n1_num, num_rect)
+                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + SQUARE_SIZE//2))
+            self.screen.blit(n1_num, num_rect)
         if self.value==2:
             num_rect=n2_num.get_rect(
-                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + self.row * SQUARE_SIZE//2))
-            screen.blit(n2_num, num_rect)
+                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + SQUARE_SIZE//2))
+            self.screen.blit(n2_num, num_rect)
 
         if self.value==3:
             num_rect=n3_num.get_rect(
-                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + self.row * SQUARE_SIZE//2))
-            screen.blit(n3_num, num_rect)
+                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + SQUARE_SIZE//2))
+            self.screen.blit(n3_num, num_rect)
 
         if self.value==4:
             num_rect=n4_num.get_rect(
-                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + self.row * SQUARE_SIZE//2))
-            screen.blit(n4_num, num_rect)
+                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + SQUARE_SIZE//2))
+            self.screen.blit(n4_num, num_rect)
 
         if self.value==5:
             num_rect=n5_num.get_rect(
-                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + self.row * SQUARE_SIZE//2))
-            screen.blit(n5_num, num_rect)
+                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + SQUARE_SIZE//2))
+            self.screen.blit(n5_num, num_rect)
 
         if self.value==6:
             num_rect=n6_num.get_rect(
-                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + self.row * SQUARE_SIZE//2))
-            screen.blit(n6_num, num_rect)
+                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + SQUARE_SIZE//2))
+            self.screen.blit(n6_num, num_rect)
 
         if self.value==7:
             num_rect=n7_num.get_rect(
-                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + self.row * SQUARE_SIZE//2))
-            screen.blit(n7_num, num_rect)
+                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + SQUARE_SIZE//2))
+            self.screen.blit(n7_num, num_rect)
 
 
         if self.value==8:
             num_rect=n8_num.get_rect(
-                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + self.row * SQUARE_SIZE//2))
-            screen.blit(n8_num, num_rect)
+                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + SQUARE_SIZE//2))
+            self.screen.blit(n8_num, num_rect)
 
 
         if self.value==9:
             num_rect=n9_num.get_rect(
-                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + self.row * SQUARE_SIZE//2))
-            screen.blit(n9_num, num_rect)
+                center=(SQUARE_SIZE * self.column + SQUARE_SIZE//2, SQUARE_SIZE * self.row + SQUARE_SIZE//2))
+            self.screen.blit(n9_num, num_rect)
 
 
 
--- sudoku.py
+++ sudoku.py
@@ -1,29 +1,90 @@
 import pygame
 import sys
+
+from pygame import Surface, SurfaceType
+
+import board
 from constants import *
 from board import Board
-import cell
-import constants
+from cell import Cell
 from sudoku_generator import SudokuGenerator
 pygame.init()
+#screen: Surface | SurfaceType = pygame.display.set_mode((WIDTH, HEIGHT))
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("SUDOKU")
-window = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
+
+"""board = Board.draw(Board)
+board[1][2]="1"
+board[2][2]="2"
+player = 1
+chip = "1"
+game_over=False
+winner=0"""
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 window.fill(BG_COLOR)
 
-cell1 = cell.Cell(4, WIDTH//2, HEIGHT//2, window)
-cell1.draw(window)
+#cell1 = Cell(4, WIDTH//2, HEIGHT//2, window)
+#cell1.draw(window)
 
 game_over_font = pygame.font.Font(None, GAME_OVER_FONT)
 game_over = False
 
+
+
+"""
+
+def draw_grid():
+    pygame.draw.line(screen, LINE_COLOR, (0, 575), (500 * SQUARE_SIZE, HEIGHT), LINE_WIDTH // 2)
+    pygame.draw.line(screen, LINE_COLOR, (0, 0), (800 * SQUARE_SIZE, HEIGHT), LINE_WIDTH // 2)
+
+    #Draw horizontal ine
+    for i in range(1, BOARD_ROWS):
+        pygame.draw.line(screen, LINE_COLOR, (0,i*SQUARE_SIZE), (WIDTH, i*SQUARE_SIZE), LINE_WIDTH//2)
+
+    #draw vertical line:
+    for i in range(1, BOARD_COLS):
+        pygame.draw.line(screen, LINE_COLOR, (i*SQUARE_SIZE, 0), (i*SQUARE_SIZE, HEIGHT-125), LINE_WIDTH//2)
+
+
+"""
+
+screen.fill(BG_COLOR)
+c = Cell(1, 1, 1, screen)
+c.draw()
+b = Board(2, 2, screen, 1)
+b.draw()
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.quit()
             sys.exit()
 
+        if event.type == pygame.KEYDOWN:
+            x,y=event.pops
+            row = y//SQUARE_SIZE
+            col = (x//SQUARE_SIZE)
 
     pygame.display.update()
+
+
+
+"""            if available_square(board, row, col):
+                mark_square(board, rowl, col, chip)
+                if check_if_winner(board, num):
+                    game_over = True
+                    winner = player
+
+                else:
+                    if board_is_full(board):
+                        game_over = True
+                        winner =0 #indicates a tie
+
+            player = 2 if player==1 else 1"""
+
+
+
 
 
 def print_hi(name):
